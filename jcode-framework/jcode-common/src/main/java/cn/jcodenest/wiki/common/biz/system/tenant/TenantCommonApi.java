9e970fb818fac55825414dd3414a2f0cedfad976
JCodeNest <jcodenest@gmail.com>
2025-07-27 23:05:23 +0800

    feat: System RPC API

---
+++ jcode-framework/jcode-common/src/main/java/cn/jcodenest/wiki/common/biz/system/tenant/TenantCommonApi.java
@@ -0,0 +1,38 @@
+package cn.jcodenest.wiki.common.biz.system.tenant;
+
+import cn.jcodenest.wiki.common.constants.RpcConstants;
+import cn.jcodenest.wiki.common.pojo.CommonResult;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+/**
+ * 多租户 API
+ *
+ * @author JCodeNest
+ * @version 1.0.0
+ * @since 2025/7/27
+ * <p>
+ * Copyright (c) 2025 JCodeNest-Cloud-Initializer
+ * All rights reserved.
+ */
+@Tag(name = "RPC 服务 - 多租户")
+@FeignClient(name = RpcConstants.SYSTEM_NAME)
+public interface TenantCommonApi {
+
+    String PREFIX = RpcConstants.SYSTEM_PREFIX + "/tenant";
+
+    @GetMapping(PREFIX + "/id-list")
+    @Operation(summary = "获得所有租户编号")
+    CommonResult<List<Long>> getTenantIdList();
+
+    @GetMapping(PREFIX + "/valid")
+    @Operation(summary = "校验租户是否合法")
+    @Parameter(name = "id", description = "租户编号", required = true, example = "1024")
+    CommonResult<Boolean> validTenant(@RequestParam("id") Long id);
+}
