76514016f626c650d1f761c0350f4665f2b3452d
JCodeNest <jcodenest@gmail.com>
2025-08-01 23:37:50 +0800

    feat: infra 模块初始化

---
+++ jcode-module-infra/jcode-module-infra-server/src/main/java/cn/jcodenest/module/infra/InfraServerApplication.java
@@ -0,0 +1,15 @@
+package cn.jcodenest.module.infra;
+
+/**
+ * 启动类
+ *
+ * @author JCodeNest
+ * @version 1.0.0
+ * @since 2025/8/1
+ * <p>
+ * Copyright (c) 2025 JCodeNest-Cloud-Initializer
+ * All rights reserved.
+ */
+public class InfraServerApplication {
+
+}
